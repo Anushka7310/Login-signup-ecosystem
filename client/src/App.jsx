b6210046db1de66580490e4fcc823777642f6fd7
Anushka7310 <anushkaguptably@gmail.com>
2023-09-28 18:37:46 +0530

    added navbar, home, login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import axios from "axios";
 
+axios.defaults.baseURL = "https://localhost:8000";
+axios.defaults.withCredentials = true;
 function App() {
   return (
     <>
-      <h1>hii</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
